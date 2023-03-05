69375b4f58434264542ad736d9c1517d9dc5032c
osawa-koki <github@osawa.dev>
2023-03-05 19:52:08 +0900

    fst commit;;

---
+++ README.md
@@ -0,0 +1,8 @@
+---
+title: simple-rc4
+description: 🐵🐵🐵 C#でRC4暗号アルゴリズムを実装したライブラリです。
+contributors:
+  - osawa-koki
+license: Apache-2.0
+created_at: 2023-03-05
+---
---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+namespace simple_rc4
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, World!");
+    }
+  }
+}
